11134ff1ec02a49803cc52e45308cff309e1c5b4
Simon Hooker <simon@mso.net>
2014-11-12 13:54:24 +0000

    Switched to using raphael drag however still haven't got the links to determine region figured out

--- js/app-logic.js
+++ js/app-logic.js
@@ -1,17 +1,13 @@
-App.BOARD = {
-	regions: App.REGIONS,
-	paper: undefined,
-	regionareas: undefined
-};
 
 App.CHAT = [];
 
-App.UNITS = [];
-
 App.GAME = {
 	chat: App.CHAT,
-	board: App.BOARD,
-	units: App.UNITS
+	board: {
+		regions: App.REGIONS,
+		paper: undefined,
+		units: undefined
+	}
 };
 
 App.ApplicationRoute = Ember.Route.extend({
@@ -41,6 +37,76 @@ App.GameRoute = Ember.Route.extend({
 			if (
 				typeof model.board.paper === 'undefined'
 			) {
+
+				model.board.paper = Raphael('raphael-game-board',1370,2274);
+				model.board.units = model.board.paper.set();
+
+				var areaHoverCallback = function(areaSet,fill,stroke) {
+					return function() {
+						areaSet.attr({
+							fill: fill,
+							stroke: stroke
+						});
+					};
+				};
+
+				$.each(model.board.regions,function(i,region){
+
+					region.area.o = model.board.paper.set();
+
+					$.each( this.area.paths , function(j,path){
+						region.area.o.push(
+							model.board.paper.path(
+								path
+							)
+						);
+					});
+
+					if (
+						typeof region.area.stroke !== 'undefined'
+					) {
+						region.area.o.attr({
+							stroke: region.area.stroke ,
+							'stroke-width': ( region.area.width || 1 )
+						});
+					} else {
+						region.area.o.attr({
+							'stroke-width': 0
+						});
+					}
+
+
+					if (
+						typeof region.name !== 'undefined'
+					) {
+						region.name.o = model.board.paper.text( 
+							region.name.x, 
+							region.name.y, 
+							region.name.text 
+						).attr({
+							'text-anchor': 'start',
+							fill: '#FFFFFF',
+							'font-size': '20px'
+						});
+					}
+
+					if (
+						typeof region.area.fill !== 'undefined'
+					) {
+						region.area.o.attr({
+							fill: region.area.fill
+						});
+/*
+						region.area.o.onDragOver(function( a,b,c,d ){
+							console.log(a,b,c,d);
+						});
+						region.area.o
+							.mouseover(areaHoverCallback(region.area.o,shadeColor(region.area.fill,0.5),(region.area.stroke==region.area.fill)?shadeColor(region.area.stroke,0.5):region.area.stroke))
+							.mouseout(areaHoverCallback(region.area.o,region.area.fill,region.area.stroke));
+*/
+
+					}
+				});
 			}
 		});
 
@@ -49,6 +115,24 @@ App.GameRoute = Ember.Route.extend({
 		return App.GAME;
 	}
 });
+
+
+var methods = {
+	unit: {
+		drag: {
+			start: function() {
+				this.ox = this.attr('cx');
+				this.oy = this.attr('cy');
+			},
+			move: function(dx,dy) {
+				this.attr({cx: this.ox + dx, cy: this.oy + dy});
+			},
+			end: function() {
+				console.log( this.paper.getElementsByPoint( this.attr('cx') , this.attr('cy') ) );
+			}
+		}
+	}
+};
 
 
 socket.on('connect',function(data){});
@@ -65,6 +149,25 @@ socket.on('spawn',function(data){
 	var frame = $('#raphael-game-board');
 
 	$.each(data,function(i,unit) {
+
+		App.GAME.board.units.push(
+			App.GAME.board.paper.circle(
+				unit.x,
+				unit.y,
+				20
+			).attr({
+				stroke: '#000000',
+				fill: '#FF0000'
+			}).drag(
+				methods.unit.drag.move,
+				methods.unit.drag.start,
+				methods.unit.drag.end
+			)
+		);
+
+
+
+		/*
 		var el = $('<div class="unit"></div>')
 		el.css({
 			top: unit.y+'px',
@@ -72,8 +175,15 @@ socket.on('spawn',function(data){
 		});
 		frame.append(el);
 		el.draggable({
-			containment: '#raphael-game-board'
+			containment: '#raphael-game-board',
+			drag: function( event , ui ) {
+			},
+			stop: function( event , ui ) {
+				console.log(ui);
+				console.log(App.GAME.board.paper.getElementByPoint( event.pageX , event.pageY ));
+			}
 		});
+		*/
 	});
 });
 
@@ -102,72 +212,6 @@ $(function(){
 
 	// Render game board
 		var model = App.GAME;
-
-		model.board.paper = Raphael('raphael-game-board',1370,2274);
-
-		model.board.regionareas = model.board.paper.set();
-
-		var areaHoverCallback = function(areaSet,fill,stroke) {
-			return function() {
-				areaSet.attr({
-					fill: fill,
-					stroke: stroke
-				});
-			};
-		};
-
-		$.each(model.board.regions,function(i,region){
-
-			region.area.o = model.board.paper.set();
-
-			$.each( this.area.paths , function(j,path){
-				region.area.o.push(
-					model.board.paper.path(
-						path
-					)
-				);
-			});
-
-			if (
-				typeof region.area.stroke !== 'undefined'
-			) {
-				region.area.o.attr({
-					stroke: region.area.stroke ,
-					'stroke-width': ( region.area.width || 1 )
-				});
-			} else {
-				region.area.o.attr({
-					'stroke-width': 0
-				});
-			}
-
-
-			if (
-				typeof region.name !== 'undefined'
-			) {
-				region.name.o = model.board.paper.text( 
-					region.name.x, 
-					region.name.y, 
-					region.name.text 
-				).attr({
-					'text-anchor': 'start',
-					fill: '#FFFFFF',
-					'font-size': '20px'
-				});
-			}
-
-			if (
-				typeof region.area.fill !== 'undefined'
-			) {
-				region.area.o.attr({
-					fill: region.area.fill
-				});
-
-				region.area.o
-					.mouseover(areaHoverCallback(region.area.o,shadeColor(region.area.fill,0.5),(region.area.stroke==region.area.fill)?shadeColor(region.area.stroke,0.5):region.area.stroke))
-					.mouseout(areaHoverCallback(region.area.o,region.area.fill,region.area.stroke));
-			}
-		});
 	
 	if (autologin) {
 		socket.emit( 'join' , 'autojoin-user' );
